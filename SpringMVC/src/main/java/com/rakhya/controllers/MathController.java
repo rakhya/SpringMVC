dc11d98524f8017f8a007e1d01ce9b2c4d661bc4
rakhya <pravarakhya.chilukuri@gmail.com>
2018-01-09 18:46:27 -0500

    Modified Add Controller to MathController

--- SpringMVC/src/main/java/com/rakhya/controllers/MathController.java
+++ SpringMVC/src/main/java/com/rakhya/controllers/MathController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rakhya.services.AddService;
+import com.rakhya.services.SubtractService;
 
 @Controller
-public class AddController {
+public class MathController {
 	
 	@RequestMapping("/add")
 	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -21,8 +22,21 @@ public class AddController {
 		AddService a = new AddService();
 		
 		int k = a.add(i, j);
-		
 		ModelAndView mv = new ModelAndView("displayAdd");
+		mv.addObject("result", k);
+		return mv;
+	}
+	
+	@RequestMapping("/subtract")
+	public ModelAndView subtract(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		int i = Integer.parseInt(request.getParameter("n1"));
+		int j = Integer.parseInt(request.getParameter("n2"));
+		
+		SubtractService a = new SubtractService();
+		
+		int k = a.subtract(i, j);
+		ModelAndView mv = new ModelAndView("displaySub");
 		mv.addObject("result", k);
 		return mv;
 	}
